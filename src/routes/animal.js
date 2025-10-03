c1c2c37184183fb69486cbe1c1844b5b198bcccd
frank <test@test.com>
2025-10-02 20:30:00 -0500

    Se crea el crud

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -9,4 +9,39 @@ router.post("/animalspost", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+router.get("/animalsget", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+//Consultar un animal por su id
+router.get("/animalsget/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+//Modificar el nombre de un animal por su id
+router.put("/animalsput/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    animalSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre, edad, tipo, fecha }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+router.delete("/animalsdel/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
 module.exports = router;
